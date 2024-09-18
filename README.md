af272bc73476c533041ace12b1f5ad72c47288b9
MDeadwiler <mdeadwiler@email.com>
2024-09-18 10:43:18 -0400

    nfl-memory-game

--- README.md
+++ README.md
@@ -2,54 +2,53 @@
 
 ![NFL](https://w0.peakpx.com/wallpaper/792/854/HD-wallpaper-nfl-football-thumbnail.jpg)
 
-## ***Rules and how to play the game***
+## **_Rules and how to play the game_**
 
-+ This NFL-themed memory game challenges players to match current NFL players hidden behind a grid of squares. Players will select squares, trying to find matching. NFL Memory Game will consist of 12 squares. To win, you must successfully match all pairs. Hit reset to try again.
+- This NFL-themed memory game challenges players to match current NFL players hidden behind a grid of squares. Players will select squares, trying to find matching. NFL Memory Game will consist of 12 squares. To win, you must successfully match all pairs. Hit reset to try again.
 
-### ***Knowledge***
+### **_Knowledge_**
 
-+ JavaScript Functions
-+ Scope
-+ conditionals
-+ CSS
-+ Some MDN (NFL)
-+ Accessability for those in need
-+ DOM
-+ New Functions - checkMactch(), flipcard()
-+ FlexBox
-+ MarkDown
+- JavaScript Functions
+- Scope
+- conditionals
+- CSS
+- Some MDN (NFL)
+- Accessability for those in need
+- DOM
+- New Functions - checkMactch(), flipcard()
+- FlexBox
+- MarkDown
 
-  #### ***Some Syntax***
+  #### **_Some Syntax_**
 
-  >> ***Example***
+  > > **_Example_**
 
 ```javascript
 function shuffle(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    }
-
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
 ```
 
->#### **Tip**
+> #### **Tip**
 
-+ Look on the NFL site to reference current and relevant players [NFL](https://www.nfl.com/)
+- Look on the NFL site to reference current and relevant players [NFL](https://www.nfl.com/)
 
-### ***Outline***
+### **_Outline_**
 
-#### ***MVP***
+#### **_MVP_**
 
-+ NFL match game where the player will have to match Hall of Fame & nfl players.
+- NFL match game where the player will have to match Hall of Fame & nfl players.
 
->> My Stretch Goals:
-   Screen shakes each round. However, each round the screen becomes more intense
-   Sped up timer each round
-   Audio voice over of the NFL player after the match
-   If th round is lost, a voice over will trigger something negative to the user.
+> > My Stretch Goals:
+> > Screen shakes each round. However, each round the screen becomes more intense
+> > Sped up timer each round
+> > Audio voice over of the NFL player after the match
+> > If th round is lost, a voice over will trigger something negative to the user.
 
 ![Raiders](https://i.pinimg.com/736x/26/50/d8/2650d84d943d19549ad2252ee18f3d80.jpg)
 
-***Go Raiders!***
+**_Go Raiders!_**
--- nfl.js
+++ nfl.js
@@ -20,110 +20,105 @@
 //Player clicks a reset button to shuffle cards and start a new game
 
 // Select DOM elements
-const resetButton = document.querySelector('.reset');
-const cards = document.querySelectorAll('.memory-card');
-const winnerMessage = document.getElementById("winner-message");
+const resetButton = document.querySelector('.reset')
+const cards = document.querySelectorAll('.memory-card')
+const winnerMessage = document.getElementById('winner-message')
 const instructions = document.querySelectorAll('.instructions')
-// Game state variables
-let playerFlippedCard = false;
-let firstCard = null;
-let secondCard = null;
-let lockBoard = false;
-let matchedPairs = 0;
-const totalPairs = cards.length / 2;
+// Game variables
+let playerFlippedCard = false
+let firstCard = null
+let secondCard = null
+let lockBoard = false
+let matchedPairs = 0
+const totalPairs = cards.length / 2
 
 console.log(instructions)
 
 // Initialize game
-reset();
-shuffle();
+reset()
+shuffle()
 
 // Function to handle card flip
-function flipCard() {
+function flipCard () {
   // Prevent interaction if the board is locked or the same card is clicked twice
-  if (lockBoard || this === firstCard) return;
+  if (lockBoard || this === firstCard) return
 
-  this.classList.add('flip');
+  this.classList.add('flip')
 
   if (!playerFlippedCard) {
     // First card click
-    playerFlippedCard = true;
-    firstCard = this;
+    playerFlippedCard = true
+    firstCard = this
   } else {
     // Second card click
-    playerFlippedCard = false;
-    secondCard = this;
-    checkMatch();
+    playerFlippedCard = false
+    secondCard = this
+    checkMatch()
   }
 }
 
 // Function to check if two flipped cards match
-function checkMatch() {
+function checkMatch () {
   if (firstCard.dataset.players === secondCard.dataset.players) {
-    disableCards();
-    checkWinner();
+    disableCards()
+    checkWinner()
   } else {
-    unflipCards();
+    unflipCards()
   }
 }
 
 // Disable cards after a match
-function disableCards() {
-  firstCard.removeEventListener('click', flipCard);
-  secondCard.removeEventListener('click', flipCard);
-  matchedPairs++;
-  reset();
+function disableCards () {
+  firstCard.removeEventListener('click', flipCard)
+  secondCard.removeEventListener('click', flipCard)
+  matchedPairs++
+  reset()
 }
 
 // Flip cards back if they do not match
-function unflipCards() {
-  lockBoard = true;
+function unflipCards () {
+  lockBoard = true
   setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
-    reset();
-  }, 400);
+    firstCard.classList.remove('flip')
+    secondCard.classList.remove('flip')
+    reset()
+  }, 400)
 }
 
-
-
 // Reset the game state
-function reset() {
-  playerFlippedCard = false;
-  lockBoard = false;
-  firstCard = null;
-  secondCard = null;
+function reset () {
+  playerFlippedCard = false
+  lockBoard = false
+  firstCard = null
+  secondCard = null
 }
 
 // Shuffle the cards
-function shuffle() {
+function shuffle () {
   cards.forEach(card => {
-    const shuffledDeck = Math.floor(Math.random() * cards.length);
-    card.style.order = shuffledDeck;
-  });
+    const shuffledDeck = Math.floor(Math.random() * cards.length)
+    card.style.order = shuffledDeck
+  })
 }
 
-
-function checkWinner() {
+function checkWinner () {
   if (matchedPairs === totalPairs) {
-
-    instructions[0].innerHTML = '<h1>CONGRATULATIONS, YOU WIN!</h1>';
-    instructions[1].innerText = '';
-    instructions[2].innerText = '';
-    instructions[3].innerText = '';
-    instructions[4].innerText = '';
+    instructions[0].innerHTML = '<h1>CONGRATULATIONS, YOU WIN!</h1>'
+    instructions[1].innerText = ''
+    instructions[2].innerText = ''
+    instructions[3].innerText = ''
+    instructions[4].innerText = ''
   }
 }
 
 // Reset the game by reloading the page
-function resetGame() {
-  window.location.reload();
+function resetGame () {
+  window.location.reload()
 }
 
 // Event listeners
-cards.forEach(card => card.addEventListener('click', flipCard));
-resetButton.addEventListener('click', resetGame);
-
+cards.forEach(card => card.addEventListener('click', flipCard))
+resetButton.addEventListener('click', resetGame)
 
 //source: https://www.youtube.com/watch?v=ZniVgo8U7ek
 //source: https://www.improvememory.org/brain-games/memory-games/happy-halloween/
@@ -143,9 +138,10 @@ const handleClick = () => {
     roundAttempt -- if there were three rounds
     matchCard()
     removeCard()
-    timerOut()
-    endGame()
+    setTimeOut()
+    winner()
     reset()
+    //window.location
 }*/
 
 /*console.log(firstCard.dataset.players);
